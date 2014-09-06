1c547ddf7daaeecb170155479fa60fcbbe988e44
Sergey Abakumoff <s.abakumoff@gmail.com>
2014-09-06 21:34:18 +0700

    trying out the new ideas

--- BikeSharing/featuresEnginering.R
+++ BikeSharing/featuresEnginering.R
@@ -1,11 +1,13 @@
 setwd('~/Documents/Projects/Kaggle/BikeSharing/')
 #setwd('f:/Projects/Kaggle/BikeSharing/')
 adjust.columns <- function(data){
-  data$datetime <- strptime(data$datetime, format='%Y-%m-%d %H:%M:%S')
-  data$month <- as.numeric(format(data$datetime, '%m'))
-  data$weekday <- as.numeric(format(data$datetime, '%w'))
-  data$hour <- as.numeric(format(data$datetime, '%H'))
-  data$year <- as.numeric(format(data$datetime, '%y'))
+  datetime <- strptime(data$datetime, format='%Y-%m-%d %H:%M:%S')
+  #data$month <- as.numeric(format(data$datetime, '%m'))
+  #data$week.day <- as.numeric(format(data$datetime, '%w'))
+  #data$hour <- as.numeric(format(data$datetime, '%H'))
+  #data$year <- as.numeric(format(data$datetime, '%y'))
+  #data$month.day <- as.numeric(format(data$datetime, '%d'))
+  data$year.month.weekday.hour <- as.factor(format(datetime, '%y.%m.%w.%H'))
   return(data)
 }
 
@@ -38,16 +40,45 @@ train.column.types <- c(test.column.types,
 train.data <- read.csv('train.csv', na.strings=missing.types, colClasses=train.column.types)
 test.data <- read.csv('test.csv', na.strings=missing.types, colClasses=test.column.types)
 
+train.data$count<-NULL
 test.data$casual <- NA
 test.data$registered <- NA
-test.data$count <- NA
+#test.data$count <- NA
 
 full.data <- rbind(train.data, test.data)
 #full.data$datetime <- as.POSIXct(full.data$datetime, format='%Y-%m-%d %H:%M:%S', tz='GMT')
-full.data <- adjust.columns(full.data)
+#full.data <- adjust.columns(full.data)
 
-full.data[full.data$temp < 15, 'temp.cat']<-'low'
-full.data[full.data$temp >= 15 & full.data$temp < 30, 'temp.cat']<-'normal'
-full.data[full.data$temp >= 30, 'temp.cat']<-'high'
-full.data$temp.cat<-as.factor(full.data$temp.cat)
+full.data <- full.data[order(as.Date(full.data$datetime, format='%Y-%m-%d %H:%M:%S')), ]
+full.data$datetime <- strptime(full.data$datetime, format='%Y-%m-%d %H:%M:%S')
+full.data$year.month.weekday.hour <- as.factor(format(full.data$datetime, '%y.%m.%w.%H'))
+median.casual <- tapply(full.data$casual, year.month.weekday.hour, median, na.rm = TRUE)
+#full.data <- adjust.columns(full.data)
 
+f<-function(dt, hour){
+  month <- format(dt, '%m')
+  week.day <- format(dt, '%w')
+  hour <- as.numeric(format(dt, '%H')) - hour
+  year <- format(dt, '%y')
+  y.m.wd.h<-paste(year, month, week.day, hour, sep='.')
+  return(median.casual[y.m.wd.h])
+}
+
+base.column <- 10
+column.after <- 11
+prev.column <- 10
+n.rows<-nrow(full.data)
+for(i in 1 : 12){
+  new.column <- paste('casual.in.-', i, '.hours', sep='')
+  
+  full.data[1:i, new.column] <- full.data[1:i, prev.column]
+  
+  full.data[(1+i) : n.rows, new.column] <- full.data[1: (n.rows-i), base.column]   
+  
+  na.subset <- full.data[is.na(full.data[, new.column]),]
+  
+  full.data[ is.na( full.data[, new.column] ), new.column ] <- mapply( function(x) f (x, i ),  full.data[ is.na( full.data[, new.column ] ), 'datetime' ] )
+  
+  
+  prev.column <- new.column
+}
