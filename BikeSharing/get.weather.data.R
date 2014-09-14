ce93ddde724dab1b0877f5e6f5830d010f976e6d
Sergey Abakumoff <s.abakumoff@gmail.com>
2014-09-14 19:43:58 +0700

    got the external weather data to try

--- BikeSharing/get.weather.data.R
+++ BikeSharing/get.weather.data.R
@@ -1,4 +1,4 @@
-dates <- seq(as.Date('2011/1/1'), as.Date('2011/1/2'), by = 'day')
+dates <- seq(as.Date('2011/1/1'), as.Date('2012/12/31'), by = 'day')
 dates.formatted <- sapply(dates, function(x) format(x, '%Y/%m/%d'))
 urls <- paste('http://www.wunderground.com/history/airport/KDCA/', dates.formatted, '/DailyHistory.html?format=1', sep='')
 
@@ -21,8 +21,22 @@ column.types <- test.column.types <- c('character', # TimeEST
 missing.types <- c("N/A", "", '-')
 
 get.data <- function(url){
+  print(url)
   data.set <- read.csv(url(url), colClasses=column.types, na.strings=missing.types)
-  weather.data.set <<- rbind(w, data.set)
+  data.set[,1] <- NULL
+  weather.data.set <<- rbind(weather.data.set, data.set)
 }
-
 sapply(urls, get.data)
+
+names(weather.data.set)[ncol(weather.data.set)] <- 'date.time'
+weather.data.set$date.time <- gsub('<br />','',weather.data.set$date.time)
+weather.data.set$date.time <- as.POSIXct(weather.data.set$date.time, tz='UTC')
+weather.data.set$date.time <- format(weather.data.set$date.time, tz='EST', usetz=TRUE)
+
+weather.data.set$date.time <- strptime(weather.data.set$date.time, format='%Y-%m-%d %H:%M:%S')
+
+weather.data.set$year <- as.numeric(format(weather.data.set$date.time, '%y'))
+weather.data.set$month <- as.numeric(format(weather.data.set$date.time, '%m'))
+weather.data.set$day <- as.numeric(format(weather.data.set$date.time, '%d'))
+weather.data.set$hour <- as.numeric(format(weather.data.set$date.time, '%H'))
+weather.data.set$weekday <- as.numeric(format(weather.data.set$date.time, '%w'))
